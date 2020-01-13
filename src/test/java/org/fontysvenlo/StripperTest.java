1b05db0a7d369168444690abe48f54cb9050f145
Pieter van den Hombergh <p.vandenhonbergh@fontys.nl>
2020-01-13 10:23:20 +0100

    adding sources and stuff

---
+++ src/test/java/org/fontysvenlo/StripperTest.java
@@ -0,0 +1,19 @@
+package org.fontysvenlo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pieter van den Hombergh {@code <p.vandenhombergh@fontys.nl>}
+ */
+public class StripperTest {
+
+    @Test
+    public void testStrip() {
+        // read file
+//        fail( "Test not implemented" );
+    }
+
+
+}
